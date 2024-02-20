585b0c45ad8b0035fcc476e24c2c1ab0cd173996
Fayzullo07 <jorayevfayzillo77@gmail.com>
2024-02-20 12:01:57 +0500

    contact

--- components/Contact/page.tsx
+++ components/Contact/page.tsx
@@ -1,7 +1,7 @@
 const Contact = () => {
     return (
 
-        <div className="bg-gray-800 text-gray-100 px-8 py-12">
+        <div className="text-gray-100 px-8 py-12">
             <div
                 className="max-w-screen-xl mt-24 px-8 grid gap-8 grid-cols-1 md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 py-16 mx-auto bg-gray-100 text-gray-900 rounded-lg shadow-lg">
                 <div className="flex flex-col justify-between">
